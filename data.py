e11cf450f31bf01e55d80341dd4a7f21cdb4df5c
Shawn Williams <shawnwilliams2002.professional@gmail.com>
2026-01-12 16:49:36 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+# File with constants
+
+URBAN_ROUTES_URL = 'https://cnt-f6cf6fae-b2c9-4ec6-a64a-b9313d295ed1.containerhub.tripleten-services.com'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 0000 4321'
+CARD_CODE = '12'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
