f546505b2168b8c3c9ca0de7e421c97ad44d9818
Shawn Williams <shawnwilliams2002.professional@gmail.com>
2026-01-12 17:34:30 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -8,16 +8,19 @@ from selenium.webdriver.support.wait import WebDriverWait
 import helpers
 
 
-class Page:
+class X_Page:
     LOCATOR = (By.ID, 'x')
     LOCATOR = (By.XPATH, 'x')
     LOCATOR = (By.CLASS, 'x')
    
    
-def __init__(self, driver):
+   def __init__(self, driver):
         self.driver = driver  # Initialize the driver
 
-    def enter_locations(self, x_text):
+       def enter_locations(self, x_text):
         self.driver.find_element(*self.X_LOCATOR).send_keys(x_text)
         WebDriverWait(self.driver, 10).until( expected_conditions.visibility_of_element_located(self.X_LOCATOR))
         self.driver.find_element(*self.CALL_BUTTON_LOCATOR).click()
+      
+      def get_phone_number_field_value(self):
+        return self.driver.find_element(*self.PHONE_BUTTON_LOCATOR).get_property("textContent")
