8fe638ed4c7f79e191013efe8a1093e86cef99ab
Shawn Williams <shawnwilliams2002.professional@gmail.com>
2026-01-12 16:46:40 -0500

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('PASS')
+    else:
+        print('FAIL')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
+in_autotests_we_trust(2, True)
