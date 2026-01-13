567e516061e8384037bc98b82224a175ec0ded7a
Shawn Williams <shawnwilliams2002.professional@gmail.com>
2026-01-13 12:10:45 -0500

    Update data.py

--- data.py
+++ data.py
@@ -5,3 +5,8 @@ WEBSITE_URL = 'https://practice.qabrains.com'
 E_VALUE = 'will_tester@gmail.com'
 P_VALUE = 'Tester123'
 FEED_VALUE = 'Hello World'
+# FORM SUB PAGE
+N_VALUE = 'Will Test'
+PN_VALUE = '123 1234 123 123'
+D_VALUE = '01/01/2026'
+# DND PAGE
