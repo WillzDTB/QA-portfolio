d3a472c0914eedee74895371d731a22de1513e09
Shawn Williams <shawnwilliams2002.professional@gmail.com>
2026-01-13 12:18:50 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -32,10 +32,10 @@ class X_Page:
       def __init__(self, driver):
             self.driver = driver  # Initialize the driver
 
-       def enter_locations(self, x_text):
+       def enter_info(self, x_text):
         self.driver.find_element(*self.X_LOCATOR).send_keys(x_text)
         WebDriverWait(self.driver, 10).until( expected_conditions.visibility_of_element_located(self.X_LOCATOR))
-        self.driver.find_element(*self.CALL_BUTTON_LOCATOR).click()
+        self.driver.find_element(*self.Y_LOCATOR).click()
       
       def get_phone_number_field_value(self):
-        return self.driver.find_element(*self.PHONE_BUTTON_LOCATOR).get_property("textContent")
+        return self.driver.find_element(*self.PN_LOCATOR).get_property("textContent")
